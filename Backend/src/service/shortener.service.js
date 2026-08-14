606fc609ddae18317b3ae1e1deff6d9da14366e4
Dinesh Badal <dineshbadal.dev@gmail.com>
2026-08-14 22:05:18 +0530

    localhost removed

--- Backend/src/service/shortener.service.js
+++ Backend/src/service/shortener.service.js
@@ -10,7 +10,7 @@ const shortenUrl = async (url) => {
   // console.log(randomId)
   const savedUrl = await Url.create({url, shortcode:randomId });
   // console.log(savedUrl);
-  const shortUrl = `https://url-shortner-three-gamma.vercel.app/${savedUrl.shortcode}`
+  const shortUrl = `https://url-shortner-three-gamma.vercel.app/api/url/${savedUrl.shortcode}`
   return shortUrl;
 }
 const expandUrl = async (shortcode) => {
