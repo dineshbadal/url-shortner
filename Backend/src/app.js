0a8c50a06ad6047541d7d0e0d88a5d5c083d055b
Dinesh Badal <dineshbadal.dev@gmail.com>
2026-08-14 18:29:02 +0530

    Cors and path fixed to connect the env file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,36 @@
 import express from "express";
 import { route } from "./routes/url.routes.js";
 import cors from "cors";
-import 'dotenv/config'
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path: path.join(__dirname, "../.env")
+});
+
 const app = express();
-const frontendHosted = process.env.FRONTEND_HOSTED;
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_HOSTED
+];
+console.log(process.env.FRONTEND_HOSTED,"This is the frontend hosted");
 app.use(
-  cors({
-    origin: frontendHosted,
-  }),
+    cors({
+        origin: allowedOrigins
+    })
 );
+
 app.use(express.json());
+
 app.use("/api", route);
+
 app.get("/", (req, res) => {
-  res.send("this application is running fine ");
+    res.send("this application is running fine");
 });
 
 export default app;
